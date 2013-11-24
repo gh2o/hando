fd5294e5c953499e5ba78ed2bf154490d610949f
gh2o <git@thegavinli.com>
2013-11-24 09:51:23 -0800

    update client

--- hando.py
+++ hando.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import json
 import os
 import time
 import urllib2
@@ -23,7 +24,7 @@ class GPIO(object):
 			return fh.read().rstrip()
 	
 	def write_attribute(self, name, value):
-		with open(os.path.join(self.path, name)) as fh:
+		with open(os.path.join(self.path, name), 'w') as fh:
 			fh.write('{}\n'.format(value))
 
 	def set_direction(self, direction):
@@ -41,15 +42,36 @@ class GPIO(object):
 class Main(object):
 
 	def initialize(self):
+
 		self.check = GPIO(5)
 		self.check.set_direction(GPIO.INPUT)
 		self.forward = GPIO(30)
 		self.forward.set_direction(GPIO.OUTPUT)
 		self.backward = GPIO(31)
 		self.backward.set_direction(GPIO.OUTPUT)
+		self.last = 'closed'
 	
 	def update(self):
-		data = json.load(urllib2.urlopen('http://hando.herokuapp.com/endpoint'))
+
+		status = {'open': self.check.get_value()}
+		jstatus = json.dumps(status)
+		jcommand = urllib2.urlopen('http://hando.herokuapp.com/endpoint', jstatus).read()
+		command = json.loads(jcommand)
+
+		state = command.get('state')
+		if state in ('open', 'closed'):
+			if state != self.last:
+				if state == 'open':
+					self.backward.set_value(False)
+					self.forward.set_value(True)
+					time.sleep(5)
+					self.forward.set_value(False)
+				if state == 'closed':
+					self.forward.set_value(False)
+					self.backward.set_value(True)
+					time.sleep(5)
+					self.backward.set_value(False)
+				self.last = state
 
 	def run(self):
 		while True:
@@ -59,3 +81,7 @@ class Main(object):
 				import traceback
 				traceback.print_exc()
 			time.sleep(1)
+
+main = Main()
+main.initialize()
+main.run()
